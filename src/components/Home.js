fe8cd9d7ac97d6657d93f68e2685c1f9fe1152cc
pat-white-code <pat.k.white@gmail.com>
2020-09-22 22:12:19 -0500

    home component fetches home info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Home = (props) => {
-  const { getListings } = props;
+  const { getListings, listings } = props;
   return (
     <div>
       <h1>Welcome to ShareBnB</h1>
       <h4>(no relation to AirBNB)</h4>
       <button onClick={getListings}>Get Listings</button>
+      {listings.map(l => (
+        <div key={l._id}>
+          <h1>{l.name}</h1>
+          <img alt={'house'} src={l.images.picture_url} />
+        </div>
+      ))}
+
     </div>
   )
 }
